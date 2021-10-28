b33a3e5991d34a2d8249df059827b5612c8bd82b
cristenicu <criste_nicu@yahoo.com>
2021-10-28 15:56:22 +0300

    🩹 Add Phone column to contacts

--- resources/js/Pages/Contacts.vue
+++ resources/js/Pages/Contacts.vue
@@ -57,6 +57,20 @@
                           tracking-wider
                         "
                       >
+                        Telefon
+                      </th>
+                      <th
+                        scope="col"
+                        class="
+                          px-6
+                          py-3
+                          text-left text-xs
+                          font-medium
+                          text-gray-500
+                          uppercase
+                          tracking-wider
+                        "
+                      >
                         Info
                       </th>
                     </tr>
@@ -68,6 +82,9 @@
                       </td>
                       <td class="px-6 py-4 whitespace-nowrap">
                         {{ contact.age }}
+                      </td>
+                      <td class="px-6 py-4 whitespace-nowrap">
+                        {{ contact.phone }}
                       </td>
                       <td class="px-6 py-4 whitespace-nowrap">
                         {{ contact.info }}
